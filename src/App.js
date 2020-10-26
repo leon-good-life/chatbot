5fcc84bf077df2f864fc3edad50caef1a9f76949
Leon Gilyadov <leon.good.life@gmail.com>
2020-10-26 14:52:18 +0200

    Add chatbot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import Messages from "./components/Messages/Messages";
+import MessageForm from "./components/MessageForm/MessageForm";
+import "./App.css";
 
-function App() {
+const initialMessages = [
+  {
+    messageText: "Hi, I'm Maya! Today you're going to help me to ace my game.",
+    isBot: true,
+    id: uuidv4(),
+  },
+];
+
+const App = () => {
+  const [messages, setMessages] = React.useState(initialMessages);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="messages-container">
+        <Messages messages={messages} />
+      </div>
+      <MessageForm {...{ messages, setMessages }} />
     </div>
   );
-}
+};
 
 export default App;
